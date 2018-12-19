2c81ae9b28a8e3719c7f697f9204e0e1ee2ac471
ToddNajera <45319361+ToddNajera@users.noreply.github.com>
2018-12-19 14:50:28 -0600

    querys de consutla de la base de datos

--- Funciones/Querys_bd.php
+++ Funciones/Querys_bd.php
@@ -4,4 +4,7 @@ Autor:Porras Najera Miguel Najera.
 Descripcion: Estan son todas las consultas que se utilizan para generar el reporte estadistico
 Tablas_bd: Polizas, Cuentas_Gastos, Cfdi_sat
 */
+/*Consulta por mes en cfdi_sat*/$query_SAT='SELECT * FROM cfdi_sat WHERE MONTH(fecha) BETWEEN "'.$mes.'" AND "'.$mes2.'"';
+/*Consulta por mes en poliza*/$query_POLIZA="";
+/*Consulta por mes en cuentas_gastos*/$query_CTAGASTOS="";
 ?>
