7fe626330b42cc0fcf9f74701f77aa9bf102484c
ToddNajera <45319361+ToddNajera@users.noreply.github.com>
2018-12-28 14:36:14 -0600

    actualizaciones de funciones y nuevas funciones

--- Funciones/funciones_php.php
+++ Funciones/funciones_php.php
@@ -16,7 +16,9 @@ function siguiente_mes($fechaIN,$fechaFN){
     return "0";
   }
 }
-
+/*
+Esta funcion es para obtener los totales de la tabla cfdi_sat
+*/
 function obtener_TOTALES_SAT($sql,$dba){
   $subtutotal_TOTAL="";
   $iva_Total="";
@@ -32,5 +34,43 @@ function obtener_TOTALES_SAT($sql,$dba){
   return $TOTALES = array($subtutotal_TOTAL,$iva_Total,$totalf_TOTAL);
 }
 
+/*
+Esta funcion es para obtener los totales de la tabla cuentas gastos
+*/
+function obtener_TOTALES_CTAGASTOS($sql,$dba){
+  $subtutotal_TOTAL="";
+  $iva_Total="";
+  $totalf_TOTAL="";
+
+  $Resultado = $dba->query($sql);
+  while( $row = $Resultado->fetch_assoc() ){
+    $subtutotal_TOTAL=$subtutotal_TOTAL+$row['base_iva'];
+    $iva_Total=$iva_Total+$row['iva_total'];
+    $totalf_TOTAL=$totalf_TOTAL+$row['total_factura'];
+  }
+
+  return $TOTALES = array($subtutotal_TOTAL,$iva_Total,$totalf_TOTAL);
+}
+
+/*
+Esta funcion es para obtener los totales de la tabla cuentas gastos
+*/
+function obtener_TOTALES_POLIZA($sql_iva,$sql_ig,$dba){
+  $subtutotal_TOTAL="";
+  $iva_Total="";
+  $totalf_TOTAL="";
+
+  $Resultado_iva = $dba->query($sql_iva);
+  while( $row = $Resultado_iva->fetch_assoc() ){
+    $iva_Total=$iva_Total+$row['cargo'];
+  }
+
+  $Resultado_ig = $dba->query($sql_ig);
+  while( $row = $Resultado_ig->fetch_assoc() ){
+    $subtutotal_TOTAL=$subtutotal_TOTAL+$row['cargo'];
+  }
+
+  return $TOTALES = array($subtutotal_TOTAL,$iva_Total,$totalf_TOTAL);
+}
 
 ?>
